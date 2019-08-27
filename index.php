34d332684880f74242666bb0683b774db2d60d29
JulienRichoz <julienrichoz@outlook.com>
2019-08-27 14:03:24 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>White Service</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.min.css" rel="stylesheet">
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+  
+  <style>
+       /* Set the size of the div element that contains the map */
+      #map {
+        height: 400px;  /* The height is 400 pixels */
+        width: 100%;  /* The width is the width of the web page */
+       }
+  </style>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">White Service</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#objectifs">Objectifs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#demarche">Démarche</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#methode">Méthode</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#collaboration">Collaboration</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+    <!-- Masthead -->
+    <header class="masthead text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <h1 class="mb-5">White Service</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+
+   <!-- Objectifs -->
+   <section id="objectifs" class="testimonials text-center bg-light">
+    <div class="container">
+
+        <h1 class="mb-5">
+          Objectifs
+        </h1>
+        <p class="lead mb-0" style="text-align:left">
+          Notre service a pour objectif de révéler le potentiel de la personne sourde ou malentendante afin d'aboutir à la mise en œuvre de ses compétences par l'accès à un emploi adapté. 
+          Nous misons sur ses forces et ses aptitudes tout en tenant compte de ses aspirations et de ses limites.
+          L’intégration en emploi permettra à cette personne de développer son autonomie et d'optimiser son insertion professionnelle et sociale.
+          A l'issue de ce processus, garant d’un maintien en emploi réussi, elle profitera ainsi à long terme de son engagement actif. 
+        </p>
+      </div>
+    </section>
+
+       <!-- Demarche -->
+   <section  id="demarche" class="testimonials text-center  white">
+    <div class="container">
+
+        <h1 class="mb-5">
+          Démarche
+        </h1>
+        <p class="lead mb-0" style="text-align:left">
+          Nos services d’intégration englobent tout le processus favorisant le développement de l’employabilité des personnes sourdes et, à terme, l’obtention d’un emploi. 
+          Les actions engagées par nos conseillers sont centrées essentiellement sur les besoins individuels de chaque individu en lien avec ses aptitudes.           
+        </p>
+      </div>
+    </section>
+
+       <!-- Méthode -->
+   <section  id="methode" class="testimonials text-center  bg-light">
+    <div class="container">
+        <h1 class="mb-5">
+          Méthode
+        </h1>
+        <p class="lead mb-0" style="text-align:left">
+          Nous procédons à une évaluation complète de la situation d’employabilité. Pour ce faire nous considérons :
+        </p>
+        <div class="container"><br>
+          <div class="row">
+              <div class="col-md-6">
+                <ul class="lead mb-0" style="text-align:left">
+                  <li >le processus d’apparition de la limitation</li>
+                  <li>les capacités résiduelles</li>
+                  <li>le niveau de formation</li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <ul class="lead mb-0" style="text-align:left">
+                  <li >l’expérience de travail</li>
+                  <li>les activités para-professionnelles éventuelles</li>
+                  <li>la situation financière et sociale de la personne</li>
+                </ul>
+              </div>
+          </div>
+          <br>
+          <p class="lead mb-0" style="text-align:left">
+            À ce stade, l'engagement actif des conseillers permet d’aider adéquatement les personnes sourdes ou malentendantes dans leur recherche d’emploi. 
+            White & White garantit un service d’aide couvrant toutes les étapes et démarches d’intégration de ces personnes.
+          </p>
+      </div>
+ 
+      </div>
+    </section>
+
+       <!-- Collaboration -->
+   <section  id="collaboration" class="testimonials text-center  white">
+    <div class="container">
+
+        <h1 class="mb-5">
+          Collaboration nécessaire
+        </h1>
+        <p class="lead mb-0" style="text-align:left">
+          Nous attendons une grande implication de la personne dans sa recherche d’emploi. 
+          Cela nous permet d'accompagner et de soutenir efficacement les entreprises tout au long du processus d’intégration du travailleur.
+        </p>
+      </div>
+    </section>
+
+
+   <!-- Contact -->
+   <section  id="contact" class="testimonials text-center  bg-light">
+    <div class="container">
+      <h1 class="mb-5">
+        Contact
+      </h1>
+      <div class="row">
+        <div class="col-md-6">
+        <h4 style="text-align: left">Adresse</h4>
+          <p style="text-align: left" class="lead mb-0">
+            <!-- Adresse // Insérer gmap et icone appel/mnail etc. -->
+            White & White Service Insertion Surdité Sàrl<br>
+            27, Rue Prévost-Martin<br>
+            1205 Geneve<br>
+            Vérène RICHOZ KANE<br><br>
+            <i class="fas fa-phone"></i> &nbsp;078 641 61 06<br>
+            <i class="fas fa-envelope"></i> &nbsp;<a href="mailto:verene.richoz@white-service.ch">verene.richoz@white-service.ch</a>
+          </p>
+        </div>
+
+        <div class="col-md-6">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2322.385461873435!2d6.1446950945343115!3d46.1945918953972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478c7ad397a7d797%3A0xc2fba686d6868ced!2sRue%20Pr%C3%A9vost-Martin%2027%2C%201205%20Gen%C3%A8ve!5e0!3m2!1sfr!2sch!4v1566899623420!5m2!1sfr!2sch" width="500" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
+
+      </div>
+    </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; White Service 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
